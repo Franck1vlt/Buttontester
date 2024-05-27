9e5d4cf4c86ba418835b77703a4b84b93be36a9b
VALMONT Franck <valmont.franck@gmail.com>
2024-05-27 10:32:11 +0400

    Ajoutez des fichiers projet.

---
+++ Buttontester/Program.cs
@@ -0,0 +1,50 @@
+using Microsoft.Extensions.Caching.Memory;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMemoryCache();
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+var app = builder.Build();
+
+
+app.UseHttpsRedirection();
+
+
+app.MapGet("/init-networks/{deviceSigfox}/{deviceLoraWan}", (string deviceSigfox, string deviceLoraWan, IMemoryCache cache) =>
+{
+    if (deviceSigfox != null)
+        cache.Remove(deviceSigfox);
+    if (deviceLoraWan != null)
+        cache.Remove(deviceLoraWan);
+    return Results.Ok();
+});
+app.MapPost("/sigfox/{device}", (string device, IMemoryCache cache) =>
+{
+    if (device != null)
+        cache.Set(device, new { Result = true });
+    return Results.Ok();
+});
+
+app.MapPost("/lorawan/{device}", (string device, IMemoryCache cache) =>
+{
+    if (device != null)
+        cache.Set(device, new { Result = true });
+    return Results.Ok();
+});
+
+app.MapGet("/sigfox/{device}", (string device, IMemoryCache cache) =>
+{
+    if (device != null)
+        return Results.Ok(cache.Get(device));
+    return Results.NotFound();
+});
+
+app.MapGet("/lorawan/{device}", (string device, IMemoryCache cache) =>
+{
+    if (device != null)
+        return Results.Ok(cache.Get(device));
+    return Results.NotFound();
+});
+
+app.MapRazorPages();
+app.Run();
